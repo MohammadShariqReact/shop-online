61b702a73d3a7a6a17c44d7d6150de6f44be9b1d
SHARIQ <mohammadshariq140389@gmail.com>
2020-06-05 10:51:38 +0530

    Updates

---
+++ precache-manifest.8fdad71cfa228d3f194ba742d80da4f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa400e1c3a133241dafa6c6200369328",
+    "url": "/shop-online/index.html"
+  },
+  {
+    "revision": "0210e90cd8c5bd3298b9",
+    "url": "/shop-online/static/css/main.af921a5f.chunk.css"
+  },
+  {
+    "revision": "033f755815d02e4cff78",
+    "url": "/shop-online/static/js/2.a3116669.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/shop-online/static/js/2.a3116669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0210e90cd8c5bd3298b9",
+    "url": "/shop-online/static/js/main.dccd13db.chunk.js"
+  },
+  {
+    "revision": "43528ed7d4736dcc8857",
+    "url": "/shop-online/static/js/runtime-main.013f7638.js"
+  },
+  {
+    "revision": "c8d83e682c33c55672a1954f3cdd2300",
+    "url": "/shop-online/static/media/crown.c8d83e68.svg"
+  }
+]);
