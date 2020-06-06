8c3d88c0ca4a5c5e28ce62966a72ed13566bf76e
SHARIQ <mohammadshariq140389@gmail.com>
2020-06-06 23:28:14 +0530

    added redux and implemented user reducer and user actions to header and app components

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -33,7 +33,7 @@ class SignUp extends React.Component{
             })
 
         } catch(error){
-            console.error(error);
+            console.log(error); 
         }
     };
     handleChange = event => {
